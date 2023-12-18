abb8a599f64a4e95e13412e605043abf7ac690df
squaredpied <prec13@hotmail.com>
2023-12-18 01:09:44 +0100

    added comments

--- potential_functions.py
+++ potential_functions.py
@@ -5,47 +5,77 @@ from matplotlib import pyplot as plt
 from PIL import Image
 import math
 
+# Function to find possible valid motions from a given position on the map
 def possible_motions(map, position):
-    x=map.shape[1]
-    y=map.shape[0]
+    # Get the dimensions of the map
+    x = map.shape[1]
+    y = map.shape[0]
 
-    possible=[]
-    if position[1]-1 >= 0 and map[position[0],position[1]-1]==0:
-        possible.append((position[0],position[1]-1))
-    if position[0]-1 >=0 and map[position[0]-1, position[1]]==0:
-        possible.append((position[0]-1,position[1]))
-    if position[1]+1 <x and map[position[0],position[1]+1]==0:
-        possible.append((position[0],position[1]+1))
-    if position[0]+1<y and map[position[0]+1,position[1]]==0:
-        possible.append((position[0]+1,position[1]))
-    if position[1]-1>=0 and position[0]-1>=0 and map[position[0]-1,position[1]-1]==0:
-        possible.append((position[0]-1,position[1]-1))
-    if position[1]+1<x and position[0]-1>=0 and map[position[0]-1,position[1]+1]==0:
-        possible.append((position[0]-1,position[1]+1))
-    if position[1]+1<x and position[0]+1<y and map[position[0]+1,position[1]+1]==0:
-        possible.append((position[0]+1,position[1]+1))
-    if position[1]-1>=0 and position[0]+1<y and map[position[0]+1,position[1]-1]==0:
-        possible.append((position[0]+1,position[1]-1))
+    # Initialize an empty list to store valid neighboring positions
+    possible = []
+
+    # Check and append valid neighboring positions based on map boundaries and obstacle-free cells
+    if position[1] - 1 >= 0 and map[position[0], position[1] - 1] == 0:
+        possible.append((position[0], position[1] - 1))
+    if position[0] - 1 >= 0 and map[position[0] - 1, position[1]] == 0:
+        possible.append((position[0] - 1, position[1]))
+    if position[1] + 1 < x and map[position[0], position[1] + 1] == 0:
+        possible.append((position[0], position[1] + 1))
+    if position[0] + 1 < y and map[position[0] + 1, position[1]] == 0:
+        possible.append((position[0] + 1, position[1]))
+    if position[1] - 1 >= 0 and position[0] - 1 >= 0 and map[position[0] - 1, position[1] - 1] == 0:
+        possible.append((position[0] - 1, position[1] - 1))
+    if position[1] + 1 < x and position[0] - 1 >= 0 and map[position[0] - 1, position[1] + 1] == 0:
+        possible.append((position[0] - 1, position[1] + 1))
+    if position[1] + 1 < x and position[0] + 1 < y and map[position[0] + 1, position[1] + 1] == 0:
+        possible.append((position[0] + 1, position[1] + 1))
+    if position[1] - 1 >= 0 and position[0] + 1 < y and map[position[0] + 1, position[1] - 1] == 0:
+        possible.append((position[0] + 1, position[1] - 1))
+
+    # Return the list of valid neighboring positions
     return possible
 
 def eucl_dis(point1,point2):
     return math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
-def wavefront_planner_euclidean(map,goal):
-    map[goal[0],goal[1]]=2
-    queue=[goal]
+# Wavefront Planner using Euclidean distance with Attraction Potential
+def wavefront_planner_euclidean(map, goal):
+    # Create an attraction potential map, initially identical to the given map
+    attract_pot = map
+    # Mark the goal cell in the attraction potential map with a value of 2
+    attract_pot[goal[0], goal[1]] = 2
+    # Initialize the queue with the goal position
+    queue = [goal]
+
+    # Continue the wavefront expansion until the queue is empty
     while len(queue) != 0:
-        new_queue=[]
-        for p in queue:
-            for m in possible_motions(map,p):
-                value=map[p[0],p[1]]+eucl_dis(m,p)
-                if map[m[0],m[1]] == 0:
-                    map[m[0],m[1]]=value
+        # Create a new queue for the next wavefront expansion
+        new_queue = []
+
+        # Iterate over each position in the current queue
+        for position in queue:
+            # Explore neighbors of the current position
+            for neighbour in possible_motions(map, position):
+                # Calculate the value for the current neighbor based on the Euclidean distance
+                value = attract_pot[position[0], position[1]] + eucl_dis(neighbour, position)
+                
+                # Update the attraction potential map with the calculated value
+                if attract_pot[neighbour[0], neighbour[1]] == 0:
+                    # If the neighbor is unexplored, set its value to the calculated value
+                    attract_pot[neighbour[0], neighbour[1]] = value
                 else:
-                    map[m[0],m[1]]=min(value,map[m[0],m[1]])
-                new_queue.append(m)
-        queue=new_queue
-    return map
+                    # If the neighbor has been visited before, update with the minimum value
+                    attract_pot[neighbour[0], neighbour[1]] = min(value, attract_pot[neighbour[0], neighbour[1]])
+
+                # Add the neighbor to the new queue for the next iteration
+                new_queue.append(neighbour)
+
+        # Update the current queue with the new queue
+        queue = new_queue
+
+    # Return the attraction potential map with updated values after wavefront expansion
+    return attract_pot
+
 
 def check_neighbours(map, position):
     x=map.shape[1]
@@ -70,32 +100,51 @@ def check_neighbours(map, position):
         possible.append((position[0]+1,position[1]-1))
     return possible
 
+# Function to perform Brushfire algorithm on a binary map
 def brushfire(map):
-    x,y=np.where(map==1)
-    queue=[]
-    for f,b in zip(x,y):
-        queue.append((f,b))
-    #print(queue)
-    while len(queue)!=0:
-        new_queue=[]
-        for p in queue:
-            for neigh in check_neighbours(map, p):
-                val=map[p[0],p[1]]+eucl_dis(neigh, p)
-                if map[neigh[0],neigh[1]] ==0:
-                    map[neigh[0],neigh[1]]=val
+    # Find the coordinates of obstacle cells (value 1) in the map
+    x, y = np.where(map == 1)
+    
+    # Initialize a queue with obstacle coordinates
+    queue = []
+    for f, b in zip(x, y):
+        queue.append((f, b))
+
+    # Continue the Brushfire algorithm until the queue is empty
+    while len(queue) != 0:
+        # Create a new queue for the next iteration
+        new_queue = []
+
+        # Iterate over each position in the current queue
+        for pos in queue:
+            # Explore neighbors of the current position
+            for neigh in possible_motions(map, pos):
+                # Calculate the value for the current neighbor based on the Euclidean distance
+                val = map[pos[0], pos[1]] + eucl_dis(neigh, pos)
+
+                # Update the map with the calculated value if the neighbor is unexplored (map value is 0)
+                if map[neigh[0], neigh[1]] == 0:
+                    map[neigh[0], neigh[1]] = val
                 else:
-                    map[neigh[0],neigh[1]]=min(map[neigh[0],neigh[1]],val)
+                    # If the neighbor has been visited before, update with the minimum value
+                    map[neigh[0], neigh[1]] = min(map[neigh[0], neigh[1]], val)
+
+                # Add the neighbor to the new queue for the next iteration
                 new_queue.append(neigh)
-        queue=new_queue
+
+        # Update the current queue with the new queue
+        queue = new_queue
+
+    # Return the map with updated values after Brushfire algorithm
     return map
 
 
 
 def find_minimum(attract_function, pos):
     minimum=pos
-    if pos[1]-1 >= 0 and attract_function[pos[0],pos[1]-1]>1 and attract_function[pos[0],pos[1]-1]<attract_function[pos[0],pos[1]]:
+    if pos[1]-1 >= 0 and attract_function[pos[0],pos[1]-1]!=1 and attract_function[pos[0],pos[1]-1]<attract_function[pos[0],pos[1]]:
         minimum=(pos[0],pos[1]-1)
-    if pos[0]-1 >=0 and attract_function[pos[0]-1, pos[1]]>1 and attract_function[pos[0]-1, pos[1]]<attract_function[minimum[0],minimum[1]]:
+    if pos[0]-1 >=0 and attract_function[pos[0]-1, pos[1]]!=1 and attract_function[pos[0]-1, pos[1]]<attract_function[minimum[0],minimum[1]]:
         minimum=(pos[0]-1,pos[1])
     if pos[1]+1<attract_function.shape[1] and attract_function[pos[0],pos[1]+1]!=1 and attract_function[pos[0],pos[1]+1]<attract_function[minimum[0],minimum[1]]:
         minimum=(pos[0],pos[1]+1)
@@ -111,18 +160,17 @@ def find_minimum(attract_function, pos):
         minimum=(pos[0]+1,pos[1]-1)
     return minimum
 
-def find_the_path(attract_function, start):
+def find_the_path(potential_function, start):
     path=[start]
     posit=start
     isGoal=False
     while isGoal == False:
-        x=find_minimum(attract_function,posit)
-        if x==posit:
+        minimum=find_minimum(potential_function,posit)
+        if minimum==posit:
             isGoal=True
         else:
-            path.append(x)
-            posit=x
-        
+            path.append(minimum)
+            posit=minimum
     return path
 
 def repulsive_function(dis_obs, Q):
@@ -131,64 +179,65 @@ def repulsive_function(dis_obs, Q):
     dis_obs[(dis_obs<=Q) & (dis_obs>1)]=4*((1/dis_obs[(dis_obs<=Q) & (dis_obs>1)])-(1/Q))**2
     return dis_obs
 
-def combining_function(attraction, repulsion):
+def potential_function(attraction, repulsion):
     attraction[attraction==1]= attraction.max()+1
     attraction=(attraction-attraction.min())/(attraction.max()-attraction.min())
     potential=attraction+repulsion
-    potential=(potential-potential.min())/(potential.max()-potential.min())    
+    # potential=(potential-potential.min())/(potential.max()-potential.min())    
     return potential
 
-# Load grid map
-image = Image.open(sys.argv[1]).convert('L')
-grid_map = np.array(image.getdata()).reshape(image.size[0],
-image.size[1])/255
-# binarize the image
-grid_map[grid_map > 0.5] = 1
-grid_map[grid_map <= 0.5] = 0
-# Invert colors to make 0 -> free and 1 -> occupied
-grid_map = (grid_map * -1) + 1
-# Show grid map
-start_point=(int(sys.argv[2]),int(sys.argv[3]))
-end_point=(int(sys.argv[4]),int(sys.argv[5]))
+if __name__=="__main__":
+    # Load grid map
+    image = Image.open(sys.argv[1]).convert('L')
+    grid_map = np.array(image.getdata()).reshape(image.size[0],
+    image.size[1])/255
+    # binarize the image
+    grid_map[grid_map > 0.5] = 1
+    grid_map[grid_map <= 0.5] = 0
+    # Invert colors to make 0 -> free and 1 -> occupied
+    grid_map = (grid_map * -1) + 1
+    # Show grid map
+    start_point=(int(sys.argv[2]),int(sys.argv[3]))
+    end_point=(int(sys.argv[4]),int(sys.argv[5]))
 
 
-X=wavefront_planner_euclidean(grid_map.copy(), end_point)
+    X=wavefront_planner_euclidean(grid_map.copy(), end_point)
+    path=find_the_path(X,start_point)
+    # print("Path = ", path)
+    dis_to_obs=brushfire(grid_map.copy())
+    repulse=repulsive_function(dis_to_obs.copy(),int(sys.argv[6]))
 
-path=find_the_path(X,start_point)
+    potential=potential_function(X,repulse)
+    # print(potential[90,70])
+    sec_path=find_the_path(potential,start_point)
+    print("Path = ", sec_path)
+    path2=potential.copy()
+    for x,y in sec_path:
+        path2[x,y]=2
 
-dis_to_obs=brushfire(grid_map.copy())
-repulse=repulsive_function(dis_to_obs.copy(),int(sys.argv[6]))
+    plt.matshow(grid_map)
+    plt.colorbar()
 
-potential=combining_function(X,repulse)
+    plt.matshow(X)
+    plt.colorbar()
 
-sec_path=find_the_path(potential,start_point)
-path2=potential.copy()
-for x,y in sec_path:
-    path2[x,y]=1
+    path1=X.copy()
+    for x,y in path:
+        path1[x,y]=0.5
+    plt.matshow(path1)
+    plt.scatter([start_point[1],end_point[1]],[start_point[0],end_point[0]],marker='+',c='r')
+    plt.colorbar()
 
-plt.matshow(grid_map)
-plt.colorbar()
+    plt.matshow(dis_to_obs)
+    plt.colorbar()
 
-plt.matshow(X)
-plt.colorbar()
+    plt.matshow(repulse)
+    plt.colorbar()
 
-path1=X.copy()
-for x,y in path:
-    path1[x,y]=0.5
-plt.matshow(path1)
-plt.scatter([start_point[1],end_point[1]],[start_point[0],end_point[0]],marker='+',c='r')
-plt.colorbar()
+    plt.matshow(potential)
+    plt.colorbar()
 
-plt.matshow(dis_to_obs)
-plt.colorbar()
-
-plt.matshow(repulse)
-plt.colorbar()
-
-plt.matshow(potential)
-plt.colorbar()
-
-plt.matshow(path2)
-plt.scatter([start_point[1],end_point[1]],[start_point[0],end_point[0]],marker='+',c=['r','g'])
-plt.colorbar()
-plt.show()
+    plt.matshow(path2)
+    plt.scatter([start_point[1],end_point[1]],[start_point[0],end_point[0]],marker='+',c=['r','g'])
+    plt.colorbar()
+    plt.show()
